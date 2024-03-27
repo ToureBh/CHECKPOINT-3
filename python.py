aa81d81d43205893caaaf7d4c9faa94fa3be73a9
ToureBh <149252642+ToureBh@users.noreply.github.com>
2024-03-27 18:49:33 +0100

    confirmar

---
+++ python.py
@@ -0,0 +1,54 @@
+#Ejercicio 1
+
+txt = "Hola"
+
+x = txt.replace("Hola", "adiós")
+
+print(x)
+
+#Ejercicio 2
+
+string = 'This is a string'
+nmbr = 100
+my_list = ['agua', 'cacao', 'azúcar']
+a_boolean = True
+
+#Ejercicio 3
+my_string = 'Hola, vamos a escribir'
+
+selection = my_string[:3]
+
+#Ejercicio 4
+my_list_now = ['Hola', 'como', 'estás']
+my_selection = my_list_now[0]
+
+
+#Ejercicio 5
+number = 100
+new_number = 100 + 10
+print(new_number)
+
+#Ejercicio 6
+my_new_list = [1, 2, 3, 4, 5]
+list_elmnt = my_new_list[-1]
+print(list_elmnt)
+
+#Ejercicio 7
+names = 'harry,alex,susie,jared,gail,conner'
+new_names = names.split(',')
+print(new_names)
+
+#Ejercicio 8
+mi_frase = 'hola, esta es mi frase'
+saludo = mi_frase[:4].upper()
+frase_completa = saludo + mi_frase[4:]
+print(frase_completa)
+
+#Ejercicio 9
+numero = 20
+mi_frase_2 = f'hola, tengo {numero} años'
+print(mi_frase_2)
+
+#Ejercicio 10
+mi_saludo = "hello world"
+print(mi_saludo)
